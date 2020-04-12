1633c55ef6344ac3d0299f37173f63c123a8e51e
DavEst91 <davidestebanmendoza@gmail.com>
2020-04-12 16:19:37 +0200

    project v1

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import sys, pandas, os, numpy
 from src.cleaning_data import *
 from argparse import ArgumentParser
+from src.obtain_old_contamination_data import *
 from src.obtain_new_contamination_data import *
 from src.obtain_data_from_AEMET import *
+
 parser = ArgumentParser(description="Este programa es para obtener información sobre la calidad del aire")
-parser.add_argument("--estacion",help="este es el número de la estacion meteorologica de la que desea obtener información meteorológica", default=49)
+parser.add_argument("--estacion",help="este es el número de la estacion meteorologica de la que desea obtener información", default=49)
+parser.add_argument("--ano",help="este es el ano desde el que desea obtener informacion en formato AAAA", default=2019)
 args = parser.parse_args()
 
 print_data_from_AEMET(get_data_from_AEMET())
-get_last_year_data(args.estacion)
+
+for i in range(int(args.ano),2020):
+	get_last_years_data(args.estacion,i)
+
 get_dataset_today(args.estacion)
 
