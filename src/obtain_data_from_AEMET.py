d0984e2a86b12a253819ee82d56c3771221fa9e2
DavEst91 <davidestebanmendoza@gmail.com>
2020-04-13 15:39:51 +0200

    obtaining AEMET data commented

--- src/obtain_data_from_AEMET.py
+++ src/obtain_data_from_AEMET.py
@@ -5,6 +5,8 @@ from src.diccionarios import *
 from dotenv import load_dotenv
 load_dotenv()
 
+
+#Function that we use to get data from AEMET, making a call to its API.
 def get_data_from_AEMET():
     url = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/28079"
     apiKey = os.getenv("AEMET_APIKEY")
@@ -14,6 +16,7 @@ def get_data_from_AEMET():
     prediccion=datos.json()
     return prediccion
 
+#This function prints on screen the data from get_data_from_AEMET()
 def print_data_from_AEMET(prediccion):
     print("Predicción meteorológica para el día de hoy del AEMET:")
     print(f"\t·El estado del cielo es {prediccion[0]['prediccion']['dia'][0]['estadoCielo'][0]['descripcion'].lower()}.")
@@ -22,7 +25,7 @@ def print_data_from_AEMET(prediccion):
     print(f"\t·La temperatura oscila entre {prediccion[0]['prediccion']['dia'][0]['temperatura']['minima']} ºC y {prediccion[0]['prediccion']['dia'][0]['temperatura']['maxima']} ºC.")
     print(f"\t·La sensación térmica oscila entre {prediccion[0]['prediccion']['dia'][0]['sensTermica']['minima']} ºC y {prediccion[0]['prediccion']['dia'][0]['sensTermica']['maxima']} ªC.")
 
-
+#This function saves the data as variables from get_data_from_AEMET()
 def store_data_from_AEMET(prediccion):
     estado_cielo=prediccion[0]['prediccion']['dia'][0]['estadoCielo'][0]['descripcion']
     hum_rel_min=prediccion[0]['prediccion']['dia'][0]['humedadRelativa']['minima']
@@ -34,6 +37,7 @@ def store_data_from_AEMET(prediccion):
     sens_temp_min=prediccion[0]['prediccion']['dia'][0]['sensTermica']['minima']
     return estado_cielo,hum_rel_min,hum_rel_max,prob_precipitacion,temp_min,temp_max,sens_temp_min,sens_temp_max
 
+#This function saves the data from get_data_from_AEMET() on a file
 def save_data_from_AEMET(prediccion,estacion,ano):
     today = date.today()
     dia,mes,ano = today.strftime("%d"),today.strftime("%m"),today.strftime("%y")
